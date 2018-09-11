686b4dcb87b1e24ad68b8cbcaec0a4a5d59e6bc0
Disconnected59 <Disconnected59@users.noreply.github.com>
2018-09-11 16:00:16 +0200

    df

--- ApplicationResponsables/ApplicationResponsables/Form1.Designer.cs
+++ ApplicationResponsables/ApplicationResponsables/Form1.Designer.cs
@@ -29,13 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(10, 10);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(200, 50);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Rafraichir";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(10, 420);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(200, 50);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Quitter";
+            this.btnQuit.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(755, 474);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnRefresh);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Tableau de bord";
@@ -45,6 +67,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnQuit;
     }
 }
 
