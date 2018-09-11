0d4e7ede3daff594268c6ebd1ccadf1ba1ab4dda
Nullos59 <35691900+Nullos59@users.noreply.github.com>
2018-09-11 17:04:06 +0200

    Commit fin de séance

--- ApplicationResponsables/ApplicationResponsables/Passerelle.cs
+++ ApplicationResponsables/ApplicationResponsables/Passerelle.cs
@@ -15,13 +15,13 @@ namespace ApplicationResponsables
         private static SqlConnection laConnection = null;
 
 
-        //Fonction de connexion
+        // ----- Fonction de connexion ------
         private static bool seConnecter()
         {
             if (laConnection == null)
             {
                 laConnection = new SqlConnection();
-                laConnection.ConnectionString = "Data Source=WIN-921C8FKTGAE;Initial Catalog=slam2019HopitalPharmacieLecompte;User ID=lecompte;Password=lecompte";
+                laConnection.ConnectionString = "Data Source=WIN-921C8FKTGAE;Initial Catalog=slam2019BearezPpeBearez;User ID=bearez;Password=bearez";
                 //laConnection.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=slam2019HopitalPharmacie;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
                 laConnection.Open();
                 System.Diagnostics.Debug.WriteLine("instanciation connexion");
@@ -37,7 +37,7 @@ namespace ApplicationResponsables
         }
 
 
-        //Fonction de déconnexion
+        // ----- Fonction de déconnexion -----
         public static bool seDeconnecter()
         {
             if (laConnection != null)
@@ -51,6 +51,14 @@ namespace ApplicationResponsables
         }
 
 
+        // ----- Fonction récupérant la capacité d'accueil maximale du service -----
+        public static bool recupCapacitéMax(String pService)
+        {
+            bool verif=false;
+
+
+            return verif;
+        }
 
 
 
