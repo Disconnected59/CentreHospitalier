e6dad1be89f4558e31927fd83846b79cff316c4e
Nullos59 <35691900+Nullos59@users.noreply.github.com>
2018-11-13 14:57:45 +0100

    Appli symfony

---
+++ GestionRendezVous/src/Entity/Consultation.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ConsultationRepository")
+ */
+class Consultation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $Date;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $Heure;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Medecin")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $idMedecin;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Patient")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $idPatient;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getHeure(): ?\DateTimeInterface
+    {
+        return $this->Heure;
+    }
+
+    public function setHeure(\DateTimeInterface $Heure): self
+    {
+        $this->Heure = $Heure;
+
+        return $this;
+    }
+
+    public function getIdMedecin(): ?Medecin
+    {
+        return $this->idMedecin;
+    }
+
+    public function setIdMedecin(?Medecin $idMedecin): self
+    {
+        $this->idMedecin = $idMedecin;
+
+        return $this;
+    }
+
+    public function getIdPatient(): ?Patient
+    {
+        return $this->idPatient;
+    }
+
+    public function setIdPatient(?Patient $idPatient): self
+    {
+        $this->idPatient = $idPatient;
+
+        return $this;
+    }
+}
---
+++ GestionRendezVous/src/Entity/Patient.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PatientRepository")
+ */
+class Patient
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $motdepasse;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $prenom;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getMotdepasse(): ?string
+    {
+        return $this->motdepasse;
+    }
+
+    public function setMotdepasse(string $motdepasse): self
+    {
+        $this->motdepasse = $motdepasse;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
