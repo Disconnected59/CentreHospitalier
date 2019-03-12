9d7279f4e2c5b157d203a68056916e7ba5cbe66b
Nullos59 <35691900+Nullos59@users.noreply.github.com>
2019-03-12 16:01:51 +0100

    Assistants

---
+++ GestionRendezVous/src/Entity/Assistant.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AssistantRepository")
+ */
+class Assistant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Service", cascade={"persist", "remove"})
+     */
+    private $idService;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getIdService(): ?Service
+    {
+        return $this->idService;
+    }
+
+    public function setIdService(?Service $idService): self
+    {
+        $this->idService = $idService;
+
+        return $this;
+    }
+}
--- bdd/bddgestionrdv.sql
+++ bdd/bddgestionrdv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 18 déc. 2018 à 15:30
--- Version du serveur :  5.7.21
--- Version de PHP :  7.2.4
+-- Généré le :  mar. 12 mars 2019 à 15:01
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,6 +27,43 @@ USE `bddgestionrdv`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `assistant`
+--
+
+DROP TABLE IF EXISTS `assistant`;
+CREATE TABLE IF NOT EXISTS `assistant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_service_id` int(11) DEFAULT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_C2997CD148D62931` (`id_service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `assistant`
+--
+
+INSERT INTO `assistant` (`id`, `id_service_id`, `nom`, `prenom`) VALUES
+(1, 1, 'Lombroi', 'Kevin'),
+(2, 2, 'Casse', 'Laure'),
+(3, 3, 'Decheval', 'Adam'),
+(4, 4, 'Douminos', 'Zineb'),
+(5, 5, 'Bearez', 'Antoine'),
+(6, 6, 'Lendore', 'Theophile'),
+(7, 7, 'Enair', 'Jessie'),
+(8, 8, 'Lavale', 'Pierre'),
+(9, 9, 'Rubine', 'Claudine'),
+(10, 10, 'Jubile', 'Jean'),
+(11, 11, 'Chong', 'Chin\r\n'),
+(12, 12, 'Poly', 'Karene'),
+(13, 13, 'Loukir', 'Paul'),
+(14, 14, 'Jesus', 'Joseph'),
+(15, 15, 'Artoung', 'Bertrand');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `consultation`
 --
 
@@ -40,6 +77,18 @@ CREATE TABLE IF NOT EXISTS `consultation` (
   PRIMARY KEY (`id`),
   KEY `IDX_964685A6A1799A53` (`id_medecin_id`),
   KEY `IDX_964685A6CE0312AE` (`id_patient_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `id_service`
+--
+
+DROP TABLE IF EXISTS `id_service`;
+CREATE TABLE IF NOT EXISTS `id_service` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -93,8 +142,7 @@ CREATE TABLE IF NOT EXISTS `migration_versions` (
 --
 
 INSERT INTO `migration_versions` (`version`) VALUES
-('20181113134528'),
-('20181120135631');
+('20190312144757');
 
 -- --------------------------------------------------------
 
@@ -112,7 +160,18 @@ CREATE TABLE IF NOT EXISTS `patient` (
   `nom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `prenom` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `patient`
+--
+
+INSERT INTO `patient` (`id`, `login`, `motdepasse`, `email`, `adresse`, `nom`, `prenom`) VALUES
+(1, 'oui', 'oui', 'oui', 'oui', 'Bernard', 'Patate'),
+(2, 'non', 'non', 'non', 'non', 'Poudlard', 'Harry'),
+(3, 'Peutetre', 'Peutetre', 'Peutetre', 'Peutetre', 'Lolita', 'Jessica'),
+(4, 'Ouio', 'Ouio', 'Ouio', 'Ouio', 'Kadjiski', 'Bertrand'),
+(5, 'Nonoui', 'Nonoui', 'Nonoui', 'Nonoui', 'Bliatsuki', 'Jacob');
 
 -- --------------------------------------------------------
 
@@ -160,6 +219,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` json NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_8D93D649F85E0677` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -168,15 +229,21 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `roles`, `password`) VALUES
-(1, 'bekir', '[\"ROLE_USER\", \"ROLE_MEDECIN\"]', '$2y$10$7pwzZ4aIdsrmKgGGa0Eqrev116T4lOfYfevfMai3sZzdFnf2IOtd.'),
-(2, 'antoine', '[\"ROLE_USER\", \"ROLE_MEDECIN\"]', '$2y$10$Wd1l0PVJsr7P.MV7o11HmeUfuBN2ihY65YMVh6oUSrCvyXd3ImySS'),
-(3, 'quentin', '[\"ROLE_USER\", \"ROLE_MEDECIN\"]', '$2y$10$z5mFEE.Oa87YC2Cc0eocAexViZxaTMHD0oK.sL.iCSBJGAOs2IzFK'),
-(4, 'test', '[\"ROLE_USER\"]', '$2y$10$Vbjae/GIkTFe5v6XnF.4WOnJ6iqNxAmc5QTRfERiCoqWzHCoH8aRG');
+INSERT INTO `user` (`id`, `username`, `roles`, `password`, `nom`, `prenom`) VALUES
+(1, 'bekir', '[\"ROLE_USER\", \"ROLE_MEDECIN\"]', '$2y$10$7pwzZ4aIdsrmKgGGa0Eqrev116T4lOfYfevfMai3sZzdFnf2IOtd.', 'Aydogdu', 'Bekir\r\n'),
+(2, 'antoine', '[\"ROLE_USER\", \"ROLE_MEDECIN\"]', '$2y$10$Wd1l0PVJsr7P.MV7o11HmeUfuBN2ihY65YMVh6oUSrCvyXd3ImySS', 'Bearez', 'Antoine'),
+(3, 'quentin', '[\"ROLE_USER\", \"ROLE_ASSISTANT\"]', '$2y$10$z5mFEE.Oa87YC2Cc0eocAexViZxaTMHD0oK.sL.iCSBJGAOs2IzFK', 'Lecompte', 'Quentin'),
+(4, 'test', '[\"ROLE_USER\"]', '$2y$10$Vbjae/GIkTFe5v6XnF.4WOnJ6iqNxAmc5QTRfERiCoqWzHCoH8aRG', 'Test', 'Test\r\n');
 
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `assistant`
+--
+ALTER TABLE `assistant`
+  ADD CONSTRAINT `FK_C2997CD148D62931` FOREIGN KEY (`id_service_id`) REFERENCES `service` (`id`);
 
 --
 -- Contraintes pour la table `consultation`
