dc1ae9260bd264b69c1356df2a07fe005cf027f0
Disconnected59 <Disconnected59@users.noreply.github.com>
2019-03-12 16:58:01 +0100

    je sais pas ceux qui se passe

---
+++ GestionRendezVous/src/Form/Inscription.php
@@ -0,0 +1,39 @@
+<?php 
+namespace App\Form;
+use App\Entity\User;
+use App\Entity\Assistant;
+use App\Entity\Medecin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class IndicationType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('username',TextType::class)
+			->add('password',TextType::class)
+			->add('nom',TextType::class)
+			->add('prenom',TextType::class)
+			->add('save',SubmitType::class,array('label'=>'Enregistrer l\'Indication'))
+			->getForm();
+			
+	
+	}
+	
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array('data_class'=>Indication::class));
+	}
+
+
+
+}
+
+
+?>
