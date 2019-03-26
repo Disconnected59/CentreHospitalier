729501fcbd1da1471d00d2df00438b250b2aee86
Disconnected59 <Disconnected59@users.noreply.github.com>
2019-03-26 15:45:13 +0100

    c

--- Android/GestionRDV/app/src/main/java/com/example/sio2018/gestionrdv/MainActivity.java
+++ Android/GestionRDV/app/src/main/java/com/example/sio2018/gestionrdv/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Spinner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
